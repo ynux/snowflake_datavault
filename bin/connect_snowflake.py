cb3f6f8b0ffbab15fc092bd7599cf4d2bbfd39ac
ynux <ynux@gmx.net>
2020-01-30 09:29:48 +0100

    one central playground database, additional schema 'metadata', and no schema connect

--- bin/connect_snowflake.py
+++ bin/connect_snowflake.py
@@ -11,14 +11,29 @@ def engine_snowflake(configfile, target):
     config = configparser.ConfigParser()
     config.read(configfile)
 
-    # target is e.g. source_db_schema, stage_db_schema, rawvault_db_schema
+    if target == 'source':
+        database = config['source']['database'],
+        schema = config['source']['schema']
+    elif target == 'staging':
+        database = config['playground']['database'],
+        schema = config['playground']['staging_schema']
+    elif target == 'rawvault':
+        database = config['playground']['database'],
+        schema = config['playground']['rawvault_schema']
+    elif target == 'metadata':
+        database = config['playground']['database'],
+        schema = config['playground']['metadata_schema']
+    elif target == 'noschema':
+        database = config['playground']['database'],
+        schema = ''
+ 
     engine = create_engine(
         'snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}'.format(
             user=config['snowflake_credentials']['user'],
             password=config['snowflake_credentials']['password'],
             account=config['snowflake_connection']['account'],
-            database=config[target]['database'],
-            schema=config[target]['schema'],
+            database=database,
+            schema=schema,
             warehouse=config['snowflake_connection']['warehouse']
         )
     )
@@ -26,8 +41,8 @@ def engine_snowflake(configfile, target):
 
 
 if __name__ == "__main__":
-    # comes back with 3.56.4 and many warnings for me
-    engine = engine_snowflake(configfile, 'source_db_schema')
+    # comes back with 4.2.1 and many warnings for me in Jan 2020
+    engine = engine_snowflake(configfile, 'source')
     try:
         connection = engine.connect()
         results = connection.execute('select current_version()').fetchone()
